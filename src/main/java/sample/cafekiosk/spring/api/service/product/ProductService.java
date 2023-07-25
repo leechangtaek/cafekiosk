06537a6be26e2b2d5ea7915ec77307674ccc07a1
leechangtaek <ckdxor1014@naver.com>
2023-07-26 00:29:00 +0900

    Presentation Layer 테스트 (1)

--- src/main/java/sample/cafekiosk/spring/api/service/product/ProductService.java
+++ src/main/java/sample/cafekiosk/spring/api/service/product/ProductService.java
@@ -2,10 +2,12 @@ package sample.cafekiosk.spring.api.service.product;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import sample.cafekiosk.spring.api.controller.product.dto.request.ProductCreateRequest;
 import sample.cafekiosk.spring.api.service.product.response.ProductResponse;
 import sample.cafekiosk.spring.domain.product.Product;
 import sample.cafekiosk.spring.domain.product.ProductRepository;
 import sample.cafekiosk.spring.domain.product.ProductSellingStatus;
+import sample.cafekiosk.spring.domain.product.ProductType;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -16,6 +18,38 @@ public class ProductService {
 
     private final ProductRepository productRepository;
 
+
+    public ProductResponse createProduct(ProductCreateRequest request) {
+        // productNumber
+        // 001 002 003 004
+        // DB 에서 마지막 저장된 Product의 상품 번호를 읽어와서 +1
+        // 009 -> 010
+
+        String nextProductNumber = createNextProductNumber();
+
+        return ProductResponse.builder()
+                .productNumber(nextProductNumber)
+                .type(request.getType())
+                .sellingStatus(request.getSellingStatus())
+                .name(request.getName())
+                .price(request.getPrice())
+                .build();
+
+    }
+    private String createNextProductNumber(){
+        String latestProductNumber = productRepository.findLatestProductNumber();
+
+        if(latestProductNumber == null){
+            return "001";
+        }
+        int latestProductNumberInt = Integer.parseInt(latestProductNumber);
+        int nextProductNumberInt = latestProductNumberInt + 1;
+
+        return String.format("%03d",nextProductNumberInt);
+    }
+
+
+
     public List<ProductResponse> getSellingProducts(){
         List<Product> products = productRepository.findAllBySellingStatusIn(ProductSellingStatus.forDisplay());
 
@@ -23,4 +57,6 @@ public class ProductService {
                 .map(ProductResponse::of)
                 .collect(Collectors.toList());
     }
+
+
 }
