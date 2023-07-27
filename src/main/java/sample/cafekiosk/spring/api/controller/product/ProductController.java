9d4a2e024c822980813be39b7e6ce345ee8badd6
leechangtaek <ckdxor1014@naver.com>
2023-07-28 00:27:31 +0900

    Presentation Layer 테스트(2)

--- src/main/java/sample/cafekiosk/spring/api/controller/product/ProductController.java
+++ src/main/java/sample/cafekiosk/spring/api/controller/product/ProductController.java
@@ -1,13 +1,17 @@
 package sample.cafekiosk.spring.api.controller.product;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import sample.cafekiosk.spring.api.ApiResponse;
 import sample.cafekiosk.spring.api.controller.product.dto.request.ProductCreateRequest;
 import sample.cafekiosk.spring.api.service.product.ProductService;
 import sample.cafekiosk.spring.api.service.product.response.ProductResponse;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @RequiredArgsConstructor
@@ -17,15 +21,14 @@ public class ProductController {
     private final ProductService productService;
 
     @PostMapping("/api/v1/products/new")
-    public void createProduct(ProductCreateRequest request){
-        productService.createProduct(request);
-
+    public ApiResponse<ProductResponse> createProduct(@Valid @RequestBody ProductCreateRequest request){
+        return ApiResponse.ok(productService.createProduct(request.toServiceRequest()));
     }
 
 
     @GetMapping("/api/vi/products/selling")
-    public List<ProductResponse> getSellingProduct(){
-        return productService.getSellingProducts();
+    public ApiResponse<List<ProductResponse>> getSellingProduct(){
+        return ApiResponse.ok(productService.getSellingProducts());
     }
 
 }
