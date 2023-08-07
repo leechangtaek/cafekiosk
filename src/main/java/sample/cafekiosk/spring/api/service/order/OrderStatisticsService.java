2c769a9030cd4b57d49dc04a492e1ad21377173a
leechangtaek <ckdxor1014@naver.com>
2023-08-07 23:05:25 +0900

    Mockito로 Stubbing하기

--- src/main/java/sample/cafekiosk/spring/api/service/order/OrderStatisticsService.java
+++ src/main/java/sample/cafekiosk/spring/api/service/order/OrderStatisticsService.java
@@ -3,6 +3,7 @@ package sample.cafekiosk.spring.api.service.order;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import sample.cafekiosk.spring.api.service.mail.MailService;
 import sample.cafekiosk.spring.domain.order.Order;
 import sample.cafekiosk.spring.domain.order.OrderRepository;
 import sample.cafekiosk.spring.domain.order.OrderStatus;
@@ -15,8 +16,9 @@ import java.util.List;
 public class OrderStatisticsService {
 
     private final OrderRepository orderRepository;
+    private final MailService mailService;
 
-    public void sendOrderStatisticsMail(LocalDate orderDate, String email){
+    public boolean sendOrderStatisticsMail(LocalDate orderDate, String email){
         //해당 일자에 결제완료된 주문들을 가져와서
         List<Order> orders = orderRepository.findOrdersBy(
                 orderDate.atStartOfDay(),
@@ -28,7 +30,18 @@ public class OrderStatisticsService {
         int totalAmount = orders.stream()
                 .mapToInt(Order::getTotalPrice)
                 .sum();
+
         //메일 전송
+        boolean result = mailService.sendMail(
+                "no-reply@cafekiosk.com"
+                ,email
+                ,String.format("[매출통계] %s",orderDate)
+                ,String.format("총 매출 합계는 %s원입니다.",totalAmount)
+        );
+        if(!result){
+            throw new IllegalArgumentException("매출 통계 메일 전송에 실패했습니다");
+        }
+        return true;
     }
 
 }
